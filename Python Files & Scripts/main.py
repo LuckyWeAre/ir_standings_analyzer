7cc5f27bd6aca713cf7e4fb79e3b64f9a0ec4041
LuckyWeAre <eric.wineland.21@gmail.com>
2020-09-07 23:54:54 -0400

    Updated main.py, additions to continue analysis of RTP standings (in progress of trying to chart points for each id over each race and calculate drops from there)

--- Python Files & Scripts/main.py
+++ Python Files & Scripts/main.py
@@ -5,6 +5,10 @@ from ir_webstats_rc_09012020.util import clean
 import private_constants as priv_cts
 from SQL_Helper.helper import SQL_Helper
 
+# Main
+"""Get CSV files from iRacing stats page for RTP series and write them to respective files"""
+
+# logging into iRacing site using Rob Crouch's fork of ir_webstats
 email = priv_cts.EMAIL
 pswd = priv_cts.IRACING_PASSWORD
 
@@ -14,13 +18,13 @@ if not iracing.logged:
     print("INVALID CREDENTIALS")
     exit()
 
-# Main
-"""Get CSV files from iRacing stats page for RTP series and write them to respective files"""
+# Specifying names of files for the CSVs downloaded
+file_names = ['00_Current', '01_Daytona', '02_Rockingham', '03_Homestead', '04_Atlanta',
+              '05_CharlotteRoval', '06_Michigan', '07_Richmond', '08_Darlington', '09_Dover',
+              '10_LasVegas', '11_CanadianTire', '12_Texas', '13_Martinsville', '14_Phoenix']
 
-file_names = ['00_Current.csv', '01_Daytona.csv', '02_Rockingham.csv', '03_Homestead.csv', '04_Atlanta.csv',
-              '05_CharlotteRoval.csv', '06_Michigan.csv', '07_Richmond.csv', '08_Darlington.csv', '09_Dover.csv',
-              '10_LasVegas.csv', '11_CanadianTire.csv', '12_Texas.csv', '13_Martinsville.csv', '14_Phoenix.csv']
-"""
+# Pulling all CSV files for the given series (Road To Pro 2020 is id=2721) by looping through iRacing site requests
+#   after each site is grabbed, writing the returned CSV file to its respective name from the list
 num_races = 14
 for i in range(num_races + 1):
     if i == 0:
@@ -28,16 +32,17 @@ for i in range(num_races + 1):
     else:
         output_resp = iracing.get_output_csv(2721, 71, race_week=i)
 
-    rtp_file = open('/home/eric/PycharmProjects/ir_standings_analyzer/Generated CSVs/' + file_names[i], 'w')
+    rtp_file = open('/home/eric/PycharmProjects/ir_standings_analyzer/Generated CSVs/' + file_names[i] + '.csv', 'w')
     rtp_file.write(output_resp)
-    rtp_file.close()"""
+    rtp_file.close()
 
-
+# Logging into local SQL database for storage of data for later analytics use
 sql = SQL_Helper(priv_cts.HOST, priv_cts.USER, priv_cts.SQL_PASSWORD, 'RTP')
 sql.open_connection()
 
-print('starting table deletion')
-commands = sql.get_commands_from_script('/home/eric/PycharmProjects/ir_standings_analyzer/SQL Files & Scripts/table_deletion.sql')
+print('starting table deletion')  # Debug
+commands = sql.get_commands_from_script(
+    '/home/eric/PycharmProjects/ir_standings_analyzer/SQL Files & Scripts/table_deletion.sql')
 # print(commands)
 for command in commands:
     try:
@@ -49,8 +54,9 @@ for command in commands:
     except IOError as msg:
         print('Command Skipped:  ', msg)
 
-print('completed table deletion, starting table creation')
-commands = sql.get_commands_from_script('/home/eric/PycharmProjects/ir_standings_analyzer/SQL Files & Scripts/table_creation.sql')
+print('completed table deletion, starting table creation')  # Debug
+commands = sql.get_commands_from_script(
+    '/home/eric/PycharmProjects/ir_standings_analyzer/SQL Files & Scripts/table_creation.sql')
 # print(commands)
 for command in commands:
     try:
@@ -63,7 +69,8 @@ for command in commands:
         print('Command Skipped:  ', msg)
 
 print('completed table creation, starting table data loading')
-commands = sql.get_commands_from_script('/home/eric/PycharmProjects/ir_standings_analyzer/SQL Files & Scripts/table_dataloading.sql')
+commands = sql.get_commands_from_script(
+    '/home/eric/PycharmProjects/ir_standings_analyzer/SQL Files & Scripts/table_dataloading.sql')
 # print(commands)
 for command in commands:
     try:
@@ -75,7 +82,43 @@ for command in commands:
     except IOError as msg:
         print('Command Skipped:  ', msg)
 
-#query_result = sql.execute_query(db, query1)
-#print(query_result)
+sql.execute_query('commit')
 
+result = sql.execute_query('SELECT CustomerID, Name FROM 00_Current ORDER BY Points DESC;')
+print(result)
+
+ids = []
+names = []
+for i in range(len(result)):
+    ids.append(result[i][0])
+
+print(ids)
+
+for i in range(len(result)):
+    names.append(result[i][1])
+
+print(names)
+
+points_by_id = []
+iter_file_names = iter(file_names)
+# get length of files for later use of when to stop iterating through for loops to only
+# include events that have been run
+print(len(iter_file_names))
+next(iter_file_names)
+# iterate through each file (skipping overall)
+for i in iter_file_names:
+    # iterate through each id
+    for j in ids:
+        # get points for that id from that event file
+        result = sql.execute_query('SELECT Points FROM ' + str(i) + ' WHERE CustomerID=' + str(j) + ';')
+        # if there are no points, it's 0, otherwise the points from that event
+        if result:
+            points_by_id.append(result[0][0])
+        else:
+            points_by_id.append(0)
+        break  # breaking after first id for debug purposes
+
+print(points_by_id)
+
+# Close DB connection at end of program
 sql.close_connection()
---
+++ SQL Files & Scripts/table_racechart_creation.sql
@@ -0,0 +1,19 @@
+CREATE TABLE 00_Current(
+    CustomerID INT NOT NULL,
+    Name VARCHAR(255) NOT NULL UNIQUE,
+    Race1Points INT NOT NULL,
+    Race2Points INT NOT NULL,
+    Race3Points INT NOT NULL,
+    Race4Points INT NOT NULL,
+    Race5Points INT NOT NULL,
+    Race6Points INT NOT NULL,
+    Race7Points INT NOT NULL,
+    Race8Points INT NOT NULL,
+    Race9Points INT NOT NULL,
+    Race10Points INT NOT NULL,
+    Race11Points INT NOT NULL,
+    Race12Points INT NOT NULL,
+    Race13Points INT NOT NULL,
+    Race14Points INT NOT NULL,
+    PRIMARY KEY (CustomerID)
+);
